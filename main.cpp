7d4dcdd0884ba88d0caa15440d46d841a2b1df16
dmitrydryakhlov <dmitrydryakhlov.mail@gmail.com>
2018-05-13 22:43:54 +0300

    Add CCS solver

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #pragma once
 #include "utils.h"
+#include <stdio.h>
 
 const int N = 6;
+const int NzU = 15;
+const int NzL = 15;
 
 int main() {
-	float** MatrixUp, **MatrixLow;
+	float* MatrixUpVal, *MatrixLowVal;
+	int *MatrixUpRow, *MatrixLowRow,
+		*MatrixUpIndx, *MatrixLowIndx;
+
 	float* bLow, *bUp, *xLow, *xUp;
 	int* SNodesLow, *SNodesUp, NodesNLow, NodesNUp;
 
 
-	mallocMatrix(&MatrixLow, N);
-	mallocMatrix(&MatrixUp, N);
-	makeBlock6x6Low(MatrixLow, N);
-	makeBlock6x6Up(MatrixUp, N);
-	printMatrix(MatrixLow, N);
-	printMatrix(MatrixUp, N);
-	//randMatrix(Matrix, N);
+	mallocMatrixCCS(&MatrixLowVal, &MatrixLowRow, &MatrixLowIndx, NzL, N);
+	mallocMatrixCCS(&MatrixUpVal, &MatrixUpRow, &MatrixUpIndx, NzU, N);
+	makeBlock6x6LowCCS(MatrixLowVal, MatrixLowRow, MatrixLowIndx, NzL, N);
+	makeBlock6x6UpCCS(MatrixUpVal, MatrixUpRow, MatrixUpIndx, NzU, N);
+
 	mallocVectorF(&xLow, N);
 	mallocVectorF(&xUp, N);
 	mallocVectorF(&bLow, N);
@@ -23,30 +27,32 @@ int main() {
 
 	for (int i = 0; i < N; i++)
 		bUp[i] = bLow[i] = i + 1;
+	
 	printVectorF(bLow, N);
 	printVectorF(bUp, N);
 
 	mallocVectorI(&SNodesLow, N);
 	mallocVectorI(&SNodesUp, N);
-	CalcSuperNodesLow(MatrixLow, &SNodesLow, N, NodesNLow);
-	CalcSuperNodesUp(MatrixUp, &SNodesUp, N, NodesNUp);
-	printVectorI(SNodesLow, NodesNLow);
-	printVectorI(SNodesUp, NodesNUp);
-	printf("%d\n", NodesNLow);
-	printf("%d\n", NodesNUp);
 
-	blockSolverLow(MatrixLow, SNodesLow, NodesNLow, xLow, bLow, N);
-	blockSolverUp(MatrixUp, SNodesUp, NodesNUp, xUp, bUp, N);
+	CalcSuperNodesLowCCS(MatrixLowVal, MatrixLowRow, MatrixLowIndx, &SNodesLow, NodesNLow, NzL, N);
+	CalcSuperNodesUpCCS(MatrixUpVal, MatrixUpRow, MatrixUpIndx, &SNodesUp, NodesNUp, NzU, N);
+
+	printf("\nNodesLow/N : %d/%d\n", NodesNLow - 1, N);
+	printVectorI(SNodesLow, NodesNLow);
+	printf("\nNodesUp/N : %d/%d\n", NodesNUp - 1, N);
+	printVectorI(SNodesUp, NodesNUp);
+
+	blockSolverLowCCS(MatrixLowVal, MatrixLowRow, MatrixLowIndx, SNodesLow, NodesNLow, xLow, bLow, N);
+	blockSolverUpCCS(MatrixUpVal, MatrixUpRow, MatrixUpIndx, SNodesUp, NodesNUp, xUp, bUp, N);
 
 	printVectorF(xLow, N);
 	printVectorF(xUp, N);
-	FreeMem(MatrixLow, N);
-	FreeMem(MatrixUp, N);
 
-	delete[]xUp;
-	delete[]xLow;
-	delete[]bLow;
-	delete[]bUp;
+	delete[]MatrixUpVal; delete[]MatrixLowVal;
+	delete[]MatrixUpRow; delete[]MatrixLowRow;
+	delete[]MatrixUpIndx;delete[]MatrixLowIndx;
+	delete[]xUp; delete[]xLow;
+	delete[]bUp; delete[]bLow;
 
 	return 0;
 }
--- utils.h
+++ utils.h
@@ -7,14 +7,36 @@ void printVectorI(int* b, int N);
 void mallocVectorF(float** b, int N);
 void mallocVectorI(int** b, int N);
 void randVector(int* b, int N);
-void mallocMatrix(float*** a, int N);
+void mallocMatrixCCS(float** AVal, int** ARow, int** AIndx, int nz, int N);
 void makeBlock6x6Low(float** a, int N);
 void makeBlock6x6Up(float** a, int N);
+void makeBlock6x6LowCCS(float *MatrixLowVal, int *MatrixLowRow, int *MatrixLowIndx, int NzL, int N);
+void makeBlock6x6UpCCS(float *MatrixUpVal, int *MatrixUpRow, int *MatrixUpIndx, int NzU, int N);
+
 void randMatrix(float** a, int N);
+void FreeMem(float **A, int n);
+
+void randMatrixLow(float** a, int N);
+void randMatrixUp(float** a, int N);
 void CalcSuperNodesLow(float** A, int** SNodes, int N, int &NodesN);
 void CalcSuperNodesUp(float** A, int** SNodes, int N, int &NodesN);
 void changeBLow(float** Matrix, int indexX, int N, float* x, float* b);
 void changeBUp(float** Matrix, int indexX, int N, float* x, float* b);
 void blockSolverLow(float** MatrixLow, int* SNodesLow, int NodesNLow, float* xLow, float* bLow, int N);
 void blockSolverUp(float** MatrixUp, int* SNodesUp, int NodesNUp, float* xUp, float* bUp, int N);
-void FreeMem(float **A, int n);
+
+void CalcSuperNodesLowCCS(float *MatrixLowVal, int *MatrixLowRow, int *MatrixLowIndx,
+	int **SNodesLow, int &NodesNLow, int N, int NzL);
+
+void CalcSuperNodesUpCCS(float *MatrixUpVal, int *MatrixUpRow, int *MatrixUpIndx,
+	int **SNodesUp, int &NodesNUp, int N, int NzU);
+
+void changeBLowCCS(float *MatrixLowVal, int *MatrixLowRow, int *MatrixLowIndx,
+	 int indexX, int N, float* x, float* b, int NzL);
+void changeBUpCCS(float *MatrixUpVal, int *MatrixUpRow, int *MatrixUpIndx,
+	int indexX, int N, float* x, float* b);
+
+void blockSolverLowCCS(float *MatrixLowVal, int *MatrixLowRow, int *MatrixLowIndx,
+	int* SNodesLow, int NodesNLow, float* xLow, float* bLow, int N);
+void blockSolverUpCCS(float *MatrixUpVal, int *MatrixUpRow, int *MatrixUpIndx,
+	int* SNodesUp, int NodesNUp, float* xUp, float* bUp, int N);
